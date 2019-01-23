57beedb9680abde3afab8c134b6c1c3a78019136
Andrew <mykge@outlook.com>
2019-01-23 11:12:32 +0800

    update

---
+++ src/DownloadSpeedTest/DownloadFileInfo.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.FileProviders;
+using System;
+using System.IO;
+
+namespace DownloadSpeedTest
+{
+    public class DownloadFileInfo : IFileInfo
+    {
+        private static readonly byte[] bytes = new byte[10240000];
+
+        public bool Exists => true;
+
+        public long Length => bytes.Length;
+
+        public string PhysicalPath => "/testfile";
+
+        public string Name => "testfile.dat";
+
+        public DateTimeOffset LastModified => DateTimeOffset.UtcNow;
+
+        public bool IsDirectory => false;
+
+        public Stream CreateReadStream()
+        {
+            return new MemoryStream(bytes);
+        }
+    }
+}
---
+++ src/DownloadSpeedTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+
+namespace DownloadSpeedTest
+{
+    public class Program
+    {
+        private static byte[] downloadBytes;
+
+        internal static byte[] DownloadBytes
+        {
+            get
+            {
+                //var bytes = new byte[downloadBytes.Length];
+                //Buffer.BlockCopy(downloadBytes, 0, bytes, 0, bytes.Length);
+                //return bytes;
+                return downloadBytes;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            downloadBytes = new byte[1024000000];
+
+            for(int i=0;i< downloadBytes.Length; i++)
+            {
+                downloadBytes[i] = (byte)0;
+            }
+
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var builder = new WebHostBuilder();
+            builder
+                .UseKestrel()
+                .UseStartup<Startup>();
+            return builder;
+        }
+    }
+}
